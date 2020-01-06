5fc91b8c9bdb881d609d4130e9f2abd003d8267f
sibrahimpa1 <sibrahimpa1@gmail.com>
2020-01-06 18:27:23 +0100

    Files for gh pages

---
+++ build/precache-manifest.e68a3fbd02a5b7ba2fe4fd375c622e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefc9a131447258c256ea224cad75128",
+    "url": "/reactCardsFiltering/index.html"
+  },
+  {
+    "revision": "b17e28dcb037f4fa3a5a",
+    "url": "/reactCardsFiltering/static/css/main.8a429df5.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/reactCardsFiltering/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactCardsFiltering/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b17e28dcb037f4fa3a5a",
+    "url": "/reactCardsFiltering/static/js/main.74cd13b0.chunk.js"
+  },
+  {
+    "revision": "8f1694fe88b6573b2928",
+    "url": "/reactCardsFiltering/static/js/runtime-main.eac6b70f.js"
+  }
+]);
